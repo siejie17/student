998dbe753f9d145872898de5123898cf7689cb7d
LING SIE JIE <siejie0617@hotmail.com>
2025-05-03 18:01:25 +0800

    Moved the firebase configuration details - 3/5

--- utils/firebaseConfig.js
+++ utils/firebaseConfig.js
@@ -2,15 +2,24 @@ import { initializeApp } from 'firebase/app';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { getFirestore } from 'firebase/firestore';
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_DATABASE_URL,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID
+} from '@env';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBGl0o9TF3EZEQsxjKeNWsaHKc5GC1b_G8",
-  authDomain: "uniexp-bfc54.firebaseapp.com",
-  databaseURL: "https://uniexp-bfc54-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "uniexp-bfc54",
-  storageBucket: "uniexp-bfc54.firebasestorage.app",
-  messagingSenderId: "376632403855",
-  appId: "1:376632403855:web:56e01d139eb2a3ffae8dfa"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  databaseURL: FIREBASE_DATABASE_URL,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID
 };
 
 // Initialize Firebase app
